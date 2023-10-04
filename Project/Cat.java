1958085c5d9077bd24981893535e284d2abeeb7a
CodingPro445 <slowbird1108@gmail.com>
2023-10-04 14:00:29 -0500

    done

--- Project/Cat.java
+++ Project/Cat.java
@@ -20,79 +20,79 @@ public class Cat extends MovableAnimatedActor
     {
         score = 0; 
         lives = 3; 
-        String[] strings = new String[10];
-        String[] strings2 = new String[10];
-        String[] walkleft = new String[10];
-        String[] idleleft = new String[10];
-        String[] fallingright = new String[8];
-        String[] fallingleft = new String[8];
-        String[] jumpingright = new String[8];
-        String[] jumpingleft = new String[8];
-        for(int i = 1; i < 11; i ++){
-            strings[i-1] = "img/cat/Walk (" + i + ").png";
+        String[] strings = new String[8];
+        String[] strings2 = new String[8];
+        String[] walkleft = new String[8];
+        String[] idleleft = new String[8];
+        String[] fallingright = new String[2];
+        String[] fallingleft = new String[2];
+        String[] jumpingright = new String[2];
+        String[] jumpingleft = new String[2];
+        for(int i = 0; i < 8; i ++){
+            strings[i] = "img/Medival King/Run/0" + i + "_Run.png";
         }
-        for(int i = 1; i < 11; i ++){
-            walkleft[i-1] = "img/cat/Walk (" + i + ").png";
+        for(int i = 0; i < 8; i ++){
+            walkleft[i] = "img/Medival King/Run/0" + i + "_Run.png";
         }
         walk = new Animation(50, strings);
         setWalkRightAnimation(walk);
-        walk.scale(100, 87);
+        walk.scale(100, 86);
         walk.setBounds(18, 5, 54, 76);
         
         walkLeft = new Animation(50, walkleft);
         setWalkLeftAnimation(walkLeft);
-        walkLeft.scale(100, 87);
+        walkLeft.scale(100, 86);
         walkLeft.mirrorHorizontally();
         walkLeft.setBounds(28, 5, 54, 76);
-        for(int i = 1; i < 11; i ++){
-            strings2[i-1] = "img/cat/Idle (" + i + ").png";
+        for(int i = 0; i < 8; i ++){
+            strings2[i] = "img/Medival King/Idle/0" + i + "_Idle.png";
         }
-        for(int i = 1; i < 11; i ++){
-            idleleft[i-1] = "img/cat/Idle (" + i + ").png";
+        for(int i = 0; i < 8; i ++){
+            idleleft[i] = "img/Medival King/Idle/0" + i + "_Idle.png";
         }
         idle = new Animation(50, strings2);
         setIdleAnimation(idle);
-        idle.scale(100, 87);
+        idle.scale(100, 86);
         idle.setBounds(18, 5, 54, 76);
         idleLeft = new Animation(50, idleleft);
         setIdleLeftAnimation(idleLeft);
         idleLeft.mirrorHorizontally();
-        idleLeft.scale(100, 87);
+        idleLeft.scale(100, 86);
         idleLeft.setBounds(28, 5, 54, 76);
         
-        for(int i = 1; i < 9; i ++){
-            fallingright[i-1] = "img/cat/Fall (" + i + ").png";
+        for(int i = 0; i < 2; i ++){
+            fallingright[i] = "img/Medival King/Fall/0" + i + "_Fall.png";
         }
         fallingRight = new Animation(50, fallingright);
         setFallingRightAnimation(fallingRight);
-        fallingRight.scale(100, 87);
+        fallingRight.scale(100, 86);
         fallingRight.setBounds(18, 5, 54, 76);
         
-        for(int i = 1; i < 9; i ++){
-            fallingleft[i-1] = "img/cat/Fall (" + i + ").png";
+        for(int i = 0; i < 2; i ++){
+            fallingleft[i] = "img/Medival King/Fall/0" + i + "_Fall.png";
         }
         fallingLeft = new Animation(50, fallingleft);
         setFallingLeftAnimation(fallingLeft);
         fallingLeft.mirrorHorizontally();
-        fallingLeft.scale(100, 87);
+        fallingLeft.scale(100, 86);
         fallingLeft.setBounds(28, 5, 54, 76);
 
         
-        for(int i = 1; i < 9; i ++){
-            jumpingright[i-1] = "img/cat/Jump (" + i + ").png";
+         for(int i = 0; i < 2; i ++){
+            jumpingright[i] = "img/Medival King/Jump/0" + i + "_Jump.png";
         }
         jumpingRight = new Animation(50, jumpingright);
         setJumpingRightAnimation(fallingRight);
-        jumpingRight.scale(100, 87);
+        jumpingRight.scale(100, 86);
         jumpingRight.setBounds(18, 5, 54, 80);
         
-        for(int i = 1; i < 9; i ++){
-            jumpingleft[i-1] = "img/cat/Jump (" + i + ").png";
+        for(int i = 0; i < 2; i ++){
+            jumpingleft[i] = "img/Medival King/Jump/0" + i + "_Jump.png";
         }
         jumpingLeft = new Animation(50, jumpingleft);
         setJumpingLeftAnimation(jumpingLeft);
         jumpingLeft.mirrorHorizontally();
-        jumpingLeft.scale(100, 87);
+        jumpingLeft.scale(100, 86);
         jumpingLeft.setBounds(28, 5, 54, 80);
         
         
