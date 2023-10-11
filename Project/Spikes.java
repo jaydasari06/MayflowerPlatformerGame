455d50702cac7ff713d69720b09197b37457f6db
Jay Dasari <djayachandra@outlook.com>
2023-10-11 12:52:19 -0500

    spikes class added

---
+++ Project/Spikes.java
@@ -0,0 +1,17 @@
+package Project;
+import mayflower.*;
+public class Spikes extends NoGravityAnimatedActor{
+
+    public Spikes(){
+        MayflowerImage img = new MayflowerImage("img/Tiles/tile39.png");
+        img.scale(25,25);
+
+        setImage(img);
+
+    }
+
+    public void act(){
+
+    }
+    
+}
