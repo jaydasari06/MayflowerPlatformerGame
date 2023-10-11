33065b79d68084876a689ce7ed449c8dfdd2b45f
Jay Dasari <djayachandra@outlook.com>
2023-10-11 13:39:56 -0500

    spikes work and king spawn updated

--- Project/Spikes.java
+++ Project/Spikes.java
@@ -5,7 +5,7 @@ public class Spikes extends NoGravityAnimatedActor{
     public Spikes(){
         MayflowerImage img = new MayflowerImage("img/Tiles/tile39.png");
         img.scale(25,25);
-
+        img.crop(0,17,25,8);
         setImage(img);
 
     }
